4077e8077c771673fa5bb508744b6d83ba01752c
funatti <funatsufumiya@annolab.com>
2018-06-25 10:27:35 +0900

    initial commit

---
+++ src/ofxAnyJson.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ofxJsonUtils.h";
+
+namespace ofx {
+	namespace AnyJson {
+		static inline ofJson loadYaml(const std::string &path, bool isInDataDir = true) {
+
+		}
+	}
+}
+
+namespace ofxAnyJson = ofx::AnyJson;
